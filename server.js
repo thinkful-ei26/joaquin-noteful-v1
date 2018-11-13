cee1c3759abf42ff7a0c78bc822d2d6769062a42
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-12 20:37:11 -0700

    GET notes list endpoints exercise

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 'use strict';
 
 const express = require('express');
-
 const data = require('./db/notes');
 
 const app = express();
 
 // ADD STATIC SERVER HERE
 
-app.listen(8080, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
+app.get('/api/notes', (req, res) => {
+  res.json(data);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  let note = data.find(item => item.id === Number(id));
+  // console.log(req.params);
+  res.json(note);
+});
+
+app
+  .listen(8080, function() {
+    console.info(`Server listening on ${this.address().port}`);
+  })
+  .on('error', err => {
+    console.error(err);
+  });
