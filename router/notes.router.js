f1f64788174dd82cb572c4b7d63ec4fd50fdade9
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-14 14:44:31 -0700

    delete route

--- router/notes.router.js
+++ router/notes.router.js
@@ -1,10 +1,11 @@
 'use strict';
+
 const express = require('express');
 const router = express.Router();
 
 const data = require('../db/notes');
-const simDB = require('../db/simDB'); 
-const notes = simDB.initialize(data); 
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
 
 router.get('/', (req, res, next) => {
   const { searchTerm } = req.query;
@@ -18,25 +19,22 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
-  notes.find(id, (err, item) =>{
-    if(err){
+  notes.find(id, (err, item) => {
+    if (err) {
       return next(err);
     }
-    if(id){
+    if (id) {
       res.json(item);
-    }else {
+    } else {
       return 'not found';
     }
-
   });
- 
 });
 
 // Post (insert) an item
 router.post('/', (req, res, next) => {
   const { title, content } = req.body;
   console.log(req.body);
-  
 
   const newItem = { title, content };
   /***** Never trust users - validate input *****/
@@ -51,15 +49,22 @@ router.post('/', (req, res, next) => {
       return next(err);
     }
     if (item) {
-      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+      res
+        .location(`http://${req.headers.host}/notes/${item.id}`)
+        .status(201)
+        .json(item);
     } else {
       next();
     }
   });
 });
 
-
-
+router.delete('/:id', (req, res, next) => {
+  notes.delete(req.params.id, function() {
+    console.log('Item deleted');
+    res.status(204).end();
+  });
+});
 
 router.put('/:id', (req, res, next) => {
   const id = req.params.id;
@@ -67,7 +72,6 @@ router.put('/:id', (req, res, next) => {
   /***** Never trust users - validate input *****/
   const updateObj = {};
   const updateFields = ['title', 'content'];
-
 
   updateFields.forEach(field => {
     if (field in req.body) {
