c797ceee38661fb2f6a945bba26105fef0f632c7
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-15 14:02:30 -0700

    promisify search function in api.js

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -3,13 +3,21 @@
 
 const api = {
 
-  search: function (query, callback) {
-    $.ajax({
+  // search: function (query, callback) {
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: '/api/notes/',
+  //     dataType: 'json',
+  //     data: query,
+  //     success: callback
+  //   });
+  // },
+  search : function (query){
+    return $.ajax({
       type: 'GET',
       url: '/api/notes/',
       dataType: 'json',
-      data: query,
-      success: callback
+      data: query
     });
   },
 
